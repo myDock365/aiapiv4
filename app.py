10362c2970754e15f8c48e15eeb13bee60f5b083
Udayashankar MS <udayashankar.ms@global-infonet.com>
2022-05-19 13:07:48 +0530

    12

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def home():
         #error = entities
         #filtered_entities = Trainer.convert_result(entities.ents)
         # return jsonify(filtered_entities)
-        return "Error "+entities
+        return "Error "+str(entities)
     except Exception as ex:
         return "Error"+ex
 
