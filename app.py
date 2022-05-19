e1fc344a6ef585297f9833b360754fb43455a28b
Udayashankar MS <udayashankar.ms@global-infonet.com>
2022-05-19 11:56:21 +0530

    5 Commit Again

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
 # on the terminal type: curl http://127.0.0.1:5000/
 # returns hello world when we use GET.
 # returns the data that we send when we use POST.
-@app.route('/', methods=['POST'])
+@app.route('/extract', methods=['POST'])
 def home():
     data = json.loads(request.data)
     model = data["model"]
